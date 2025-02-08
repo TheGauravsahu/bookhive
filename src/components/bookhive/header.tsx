ccb66bcc74a2ebd9a223b7cc7cb2232c4b2748a3
Gaurav Sahu <itsgauravsahu7@gmail.com>
2025-02-08 16:41:20 +0530

    feat : add shad cn and prisma

--- src/components/bookhive/header.tsx
+++ src/components/bookhive/header.tsx
@@ -1,10 +1,15 @@
+import { Toggle } from "./toggle";
+
 export default function Header() {
   return (
     <header className="flex justify-between items-center px-6 py-4 md:px-12">
       <h1 className="text-3xl font-bold">BookHive 📚</h1>
-      <button className="bg-indigo-500 hover:bg-indigo-600 py-1 px-2 rounded-lg">
-        Get Started
-      </button>
+      <div>
+        <Toggle />
+        <button className="bg-indigo-500 hover:bg-indigo-600 py-1 px-2 rounded-lg">
+          Get Started
+        </button>
+      </div>
     </header>
   );
 }
