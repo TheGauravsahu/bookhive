2b76f916510ef6f5e26bcbc87b934420ee657a5c
Gaurav Sahu <itsgauravsahu7@gmail.com>
2025-02-16 19:17:52 +0530

    feat : add mentions to the footer ❤️

--- src/components/bookhive/footer.tsx
+++ src/components/bookhive/footer.tsx
@@ -6,9 +6,16 @@ export default function Footer() {
     <footer className="w-full border-t p-4 flex gap-4 flex-col md:flex-row  items-center justify-between px-16">
       <div>
         <h1 className="text-7xl font-semibold">Bookhive</h1>
-        <p className="mt-1 text-sm text-gray-400">
-          © All copyright right reserved Beehive
-        </p>
+        <a
+          target="_blank"
+          href="https://gauravsahu.vercel.app/"
+          className="mt-1 text-sm text-gray-400 cursor-pointer"
+        >
+          © All copyright right reserved Bookhive
+        </a>
+      </div>
+      <div>
+        <a target="_blank" className="hover:underline cursor-pointer" href="https://gauravsahu.vercel.app/">Designed and Developed with ❤️ by Gaurav Sahu </a>
       </div>
       <div className="flex gap-4 ">
         <Link href="/">Home</Link>
