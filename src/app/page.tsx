18e92d4da5948dcfda9f2a045e68632a575f2862
Gaurav Sahu <itsgauravsahu7@gmail.com>
2025-02-16 18:39:23 +0530

    feat : improved landing page 🔥

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="min-h-screen ">
@@ -5,14 +7,23 @@ export default function Home() {
         <h2 className="text-4xl font-bold md:text-6xl">
           Discover. Review. Recommend.
         </h2>
-        <p className="mt-4 text-lg md:text-xl text-gray-600 max-w-2xl">
+        <p className="mt-4 text-lg md:text-xl text-foreground/80 max-w-2xl">
           Your ultimate destination for book lovers. Read reviews, rate books,
           and share recommendations with the community.
         </p>
+
+        <div className="h-96 w-full relative my-4 md:mt-4 rounded-lg">
+          <Image
+            src="/bookhive-dark.png"
+            alt="bookhive"
+            fill
+            className="object-contain  rounded-lg"
+          />
+        </div>
       </main>
 
-      <section className="mt-24 px-6 md:px-12">
-        <h3 className="text-2xl font-semibold text-center">
+      <section className="md:mt-24 px-6 md:px-12 md:h-96 h-[70vh] flex md:items-center md:justify-center flex-col">
+        <h3 className="text-3xl font-semibold text-center">
           Why Choose BookHive?
         </h3>
         <div className="grid md:grid-cols-3 gap-8 mt-8">
@@ -27,6 +38,28 @@ export default function Home() {
           <FeatureCard
             title="Community Driven"
             description="Engage with fellow book lovers, share insights, and discover hidden gems."
+          />
+        </div>
+      </section>
+
+      <section className="flex md:flex-row flex-col items-center  w-full h-[60vh] md:h-screen px-8 md:gap-4 ">
+        <div className="md:w-[90%]">
+          <h1 className="font-bold text-4xl text-white mb-4">
+            Explore BookHive
+          </h1>
+          <p className="text-lg text-gray-300 max-w-2xl">
+            Discover, review, and rate books effortlessly with BookHive. Our
+            intuitive interface makes it easy to find and share your favorite
+            reads.
+          </p>
+        </div>
+
+        <div className="md:h-96 h-80 w-full relative my-6 rounded-lg shadow-lg overflow-hidden md:border">
+          <Image
+            src="/bookhive-demo.png"
+            alt="BookHive Demo"
+            fill
+            className="object-contain rounded-lg"
           />
         </div>
       </section>
